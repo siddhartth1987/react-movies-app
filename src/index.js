88aa77b70ce1dfebe926105334d793da983bb7f3
siddhartth1987 <siddhartth.1987@gmail.com>
2021-04-28 22:05:59 +0530

    Added registerServiceWorker and modified index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Home from './screens/home/Home';
+import Home from './screens/Home/Home';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
